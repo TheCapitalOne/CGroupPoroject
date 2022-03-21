4f7b8ec2c3cf8ec2dab10e0cd9a6b1d9afc48903
Zachary Bundy <99979113+TheCapitalOne@users.noreply.github.com>
2022-03-21 08:46:10 -0400

    Add files via upload

---
+++ GroupProject/Socket.h
@@ -0,0 +1,14 @@
+/*
+ * Socket.h
+ *
+ *  Created on: Mar 9, 2022
+ *      Author: djc7941, znb34
+ */
+
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+//run Open Socket prototype
+int OpenSocket(int sockfd);
+
+#endif /* SOCKET_H_ */
